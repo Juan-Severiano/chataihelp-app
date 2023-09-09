464ae070997ca749c128ae9b12a0d5fbc92cc927
Juan Severiano <juansoussev@gmail.com>
2023-09-09 20:13:30 -0300

    Input de usuário adicionado e estilizado

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import {  StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Ionicons from '@expo/vector-icons/Ionicons'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  state = {
+    userInput: '',
+  }
+
+
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1,  justifyContent: 'flex-end', backgroundColor: '#1e1e1e' }}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.userInput}
+            placeholder='Digite sua dúvida ...'
+            value={this.state.userInput}
+            onChangeText={text => this.setState({ userInput: text })}
+            placeholderTextColor='#cecece'
+          />
+          <TouchableOpacity style={styles.sendButton}>
+            <Ionicons color='#fff' name='send' size={30} />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  userInput: {
+    backgroundColor: '#151515',
+    borderRadius: 40,
+    padding: 20,
+    width: '75%',
+    color: '#fff',
+    margin: 10
+  },
+  sendButton: {
+    padding: 20,
+    borderRadius: 40,
+    backgroundColor: '#0eaa83'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+    marginBottom: 20
+  }
+})
