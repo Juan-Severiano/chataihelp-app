d1f416122035041285a33dd7b0b1d1543a35a81b
juansev <juansoussev@gmail.com>
2023-09-28 12:02:27 -0300

    REFACTOR: Lógica de conversa aplicada

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View, StatusBar } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StatusBar,
+} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import axios from 'axios';
@@ -8,35 +15,68 @@ import ResponseContainer from './components/responseContainer';
 
 export default function App() {
   const [userInput, setUserInput] = useState('');
-  const [chatResponse, setChatResponse] = useState('AI: Seja bem vindo a nossa plataforma');  
-  
+  const [chatResponse, setChatResponse] = useState(
+    'AI: Seja bem vindo a nossa plataforma'
+  );
+  const [messages, setMessages] = useState([]);
+
+  const saveUserMessages = () => {
+    if (userInput.trim() === '') {
+      return; // Evita adicionar mensagens em branco à lista
+    }
+
+    const msgContent = {
+      user: true,
+      msg: userInput,
+    };
+
+    setMessages([...messages, msgContent]);
+    setUserInput('')
+  };
+
   return (
     <>
-    <SafeAreaView style={{ flex: 1, alignItems: 'center', backgroundColor: '#1e1e1e' }}>
-      <StatusBar barStyle="light-content" />
-      <Header />
-      <View style={styles.chatContainer}>
-        <View style={{ width: '100%' }}>
-          <ResponseContainer msg={chatResponse} />
-          <ResponseContainer msg={chatResponse} userMsg />
+      <SafeAreaView style={{ flex: 1, alignItems: 'center', backgroundColor: '#1e1e1e' }}>
+        <StatusBar barStyle="light-content" />
+        <Header />
+        <View style={styles.chatContainer}>
+          <View style={{ width: '100%', zIndex: -2 }}>
+          <ResponseContainer
+                key={8}
+                msg='Olá, seja bem vindo, me diga sua dúvida'
+                userMsg={false}
+              />
+            {messages.map((item, index) => (
+              <ResponseContainer
+                key={index}
+                msg={item.msg}
+                userMsg={item.user}
+              />
+            ))}
+          </View>
+          
         </View>
-        <View style={styles.inputContainer}>
-          <TextInput
-            style={styles.userInput}
-            placeholder='Digite sua dúvida ...'
-            value={userInput}
-            onChangeText={text => setUserInput(text)}
-            placeholderTextColor='#cecece'
-          />
-          <TouchableOpacity style={styles.sendButton}>
-            <Ionicons color='#fff' name='send' size={30} />
-          </TouchableOpacity>
-        </View>
+        
+      </SafeAreaView>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.userInput}
+          placeholder='Digite sua dúvida ...'
+          value={userInput}
+          onChangeText={text => setUserInput(text)}
+          placeholderTextColor='#cecece'
+        />
+        <TouchableOpacity
+          style={styles.sendButton}
+          onPress={saveUserMessages}
+        >
+          <Ionicons color='#fff' name='send' size={30} />
+        </TouchableOpacity>
       </View>
-    </SafeAreaView>
     </>
   );
 }
+
 
 const styles = StyleSheet.create({
   chatContainer: {
@@ -61,9 +101,13 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: 'row',
     width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: '87%',
     marginBottom: 20,
+    zIndex: 2,
+    position: 'absolute',
   },
   responseContainer: {
     width: '60%',
